b4c5c8ba5d91e985a17077d69805cf47877ef440
tranhuyquang <71079726+tranhuyquang@users.noreply.github.com>
2020-09-15 19:25:27 +0700

    Add files via upload

---
+++ var.js
@@ -0,0 +1,19 @@
+        // var z;
+        // let x;
+        // let pi;
+        // z=5;
+        // pi=3.14;
+        // console.log(z);
+        // console.log(pi);
+        
+        // x=Math.pow(z,2)*pi;
+        // y=2*z*pi;
+        // console.log(x);
+
+        // let name = prompt("nhập tên");
+        // let age = prompt(" nhập tuổi");
+        // console.log("xin chào "+name);
+        // console.log("bạn "+ age +" bằng tuổi tôi");
+        let yearbirth = prompt("nhập năm sinh của bạn");
+        age = 2020-yearbirth;
+      console.log(age);
