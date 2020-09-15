346663ffcd3a270f694a5f1cb2c8e84ca1af2898
tranhuyquang <71079726+tranhuyquang@users.noreply.github.com>
2020-09-15 22:31:53 +0700

    bai tap2v3

--- datatype.js
+++ datatype.js
@@ -19,15 +19,24 @@
 // console.log(" diện tích " + R.toFixed(3));
 // console.log(" chu vi "+d.toFixed(3));
 
-let tien= prompt("nhập vào số tiền ")
-so1= (tien)/100000;
-sodu1 =(tien)%100000;
-console.log("được " + so1 + " tờ 100000vnd");
-so2=(sodu1)/50000;
-sodu2=(sodu1)%50000;
-console.log("được "+ so2 +"tờ tiền 50000vnd");
-so3=(sodu2)/20000;
-sodu3=(sodu2)%20000;
-console.log("được "+ so3 +"tờ 20000vnd");
-so4=sodu3/10000;
-console.log("được "+ so4 +"tờ 10000vnd");
+// let tien= prompt("nhập vào số tiền ");
+// so1= (tien)/100000;
+// sodu1 =(tien)%100000;
+// console.log("được " + so1 + " tờ 100000vnd");
+// so2=(sodu1)/50000;
+// sodu2=(sodu1)%50000;
+// console.log("được "+ so2 +"tờ tiền 50000vnd");
+// so3=(sodu2)/20000;
+// sodu3=(sodu2)%20000;
+// console.log("được "+ so3 +"tờ 20000vnd");
+// so4=sodu3/10000;
+// console.log("được "+ so4 +"tờ 10000vnd");
+
+
+
+let x=prompt("nhập vào số x");
+let z=prompt("nhập vào số z");
+y=4*( Math.pow(x,2)+10*x*Math.sqrt(x)+3*x+1);
+console.log("y = "+y)
+y2=( Math.sin(Math.PI*Math.pow(z,2))+Math.sqrt(Math.pow(z,2)+1) )/( Math.pow(Math.E,2*z)+ Math.cos( (Math.PI*z)/4 ) );
+console.log("y2 = " +y2);
